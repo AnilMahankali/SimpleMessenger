89e23228a1ccfbbb8f63940a6f5b7bdb33a8b352
Anil_Kumar_Mahankali <manilkumar@prokarma.com>
2016-05-02 19:32:36 +0530

    Updated with latest changes

--- app/src/main/java/com/simplemessenger/MainActivity.java
+++ app/src/main/java/com/simplemessenger/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -14,19 +15,31 @@ import android.telephony.TelephonyManager;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.net.InetAddress;
+import java.net.NetworkInterface;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.Enumeration;
 import java.util.jar.Manifest;
 
 import static android.Manifest.permission.READ_CONTACTS;
@@ -35,9 +48,13 @@ import static android.Manifest.permission.READ_SMS;
 public class MainActivity extends Activity
 {
     static final String TAG = MainActivity.class.getSimpleName();
-    static final String REMOTE_PORT0 = "11108";
-    static final String REMOTE_PORT1 = "11112";
+    static final String REMOTE_PORT0 = "9999";
+    static final String REMOTE_PORT1 = "8888";
     static final int SERVER_PORT = 8888;
+
+
+    EditText editText;
+    TextView textView;
 
     /** Called when the Activity is first created. */
     @Override
@@ -57,24 +74,7 @@ public class MainActivity extends Activity
         System.out.println("In onCreateMethod*****");
 
 
-        // Assume thisActivity is the current activity
-        int permissionCheck = ContextCompat.checkSelfPermission(this,
-                android.Manifest.permission.READ_SMS);
 
-
-        /*
-         * Calculate the port number that this AVD listens on.
-         * It is just a hack that I came up with to get around the networking limitations of AVDs.
-         * The explanation is provided in the PA1 spec.
-         */
-        //TelephonyManager tel = (TelephonyManager) this.getSystemService(Context.TELEPHONY_SERVICE);
-        //System.out.println("tel**"+tel);
-        //System.out.println("tel**"+tel.getLine1Number());
-        //String portStr = tel.getLine1Number().substring(tel.getLine1Number().length() - 4);
-        //final String myPort = String.valueOf((Integer.parseInt(portStr) * 2));
-        final String myPort = "1067";
-
-        System.out.println("my Port*****" + myPort);
         try {
             /*
              * Create a server socket as well as a thread (AsyncTask) that listens on the server
@@ -87,133 +87,61 @@ public class MainActivity extends Activity
             ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
             new ServerTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, serverSocket);
         } catch (IOException e) {
-            /*
-             * Log is a good way to debug your code. LogCat prints out all the messages that
-             * Log class writes.
-             *
-             * Please read http://developer.android.com/tools/debugging/debugging-projects.html
-             * and http://developer.android.com/tools/debugging/debugging-log.html
-             * for more information on debugging.
-             */
-            Log.e(TAG, "Can't create a ServerSocket",e);
-            return;
-        } catch (Exception e){
-            System.out.println("IN ActivityMain ERROR******"+e);
+            Log.e(TAG, "Can't create a ServerSocket");
+            // return;
         }
 
-        /*
-         * Retrieve a pointer to the input box (EditText) defined in the layout
-         * XML file (res/layout/main.xml).
-         *
-         * This is another example of R class variables. R.id.edit_text refers to the EditText UI
-         * element declared in res/layout/main.xml. The id of "edit_text" is given in that file by
-         * the use of "android:id="@+id/edit_text""
-         */
-        final EditText editText = (EditText) findViewById(R.id.edit_text);
-        System.out.println("EDIT_TEXT");
+        editText = (EditText)findViewById(R.id.edit_message);
 
-        /*
-         * Register an OnKeyListener for the input box. OnKeyListener is an event handler that
-         * processes each key event. The purpose of the following code is to detect an enter key
-         * press event, and create a client thread so that the client thread can send the string
-         * in the input box over the network.
-         */
-        try {
-            editText.setOnKeyListener(new View.OnKeyListener() {
-                @Override
-                public boolean onKey(View v, int keyCode, KeyEvent event) {
-                    //System.out.println("In setOnKeyListener----3*******" + keyCode);
-                    //System.out.println("In setOnKeyListener----4*******" + KeyEvent.KEYCODE_ENTER);
-                    if ((event.getAction() != KeyEvent.ACTION_DOWN) &&
-                            (keyCode == KeyEvent.KEYCODE_ENTER)) {
-                        System.out.println("In setOnKeyListener----1--******" + event.getAction());
-                        System.out.println("In setOnKeyListener----2*******" + KeyEvent.ACTION_DOWN);
-                        System.out.print("Key is pressed");
-                    /*
-                     * If the key is pressed (i.e., KeyEvent.ACTION_DOWN) and it is an enter key
-                     * (i.e., KeyEvent.KEYCODE_ENTER), then we display the string. Then we create
-                     * an AsyncTask that sends the string to the remote AVD.
-                     */
+        Button onClick = (Button)findViewById(R.id.button);
+        onClick.setOnClickListener(sendMessage);
 
-                        String msg = editText.getText().toString() + "\n";
-                                System.out.println("msg Typed****"+msg);
-                        editText.setText(""); // This is one way to reset the input box.
-                        TextView localTextView = (TextView) findViewById(R.id.local_text_display);
-                                System.out.println("TEXT_VIEW***"+msg);
-                        localTextView.append("\t" + msg); // This is one way to display a string.
-                        //localTextView.setText(msg);
-
-                        TextView remoteTextView = (TextView) findViewById(R.id.remote_text_display);
-                        remoteTextView.append("\n");
-                        System.out.println("In onKeyListner");
-
-                    /*
-                     * Note that the following AsyncTask uses AsyncTask.SERIAL_EXECUTOR, not
-                     * AsyncTask.THREAD_POOL_EXECUTOR as the above ServerTask does. To understand
-                     * the difference, please take a look at
-                     * http://developer.android.com/reference/android/os/AsyncTask.html
-                     */
-                        new ClientTask().executeOnExecutor(AsyncTask.SERIAL_EXECUTOR, msg, myPort);
-                        return true;
-                    }
-                    return false;
-                }
-            });
-        } catch (Exception e) {
-            System.out.println("EVENT ON KEY" + e);
-        }
-
+        textView = (TextView)findViewById(R.id.local_text_display);
     }
 
-                /***
-                 * ServerTask is an AsyncTask that should handle incoming messages. It is created by
-                 * ServerTask.executeOnExecutor() call in SimpleMessengerActivity.
-                 *
-                 * Please make sure you understand how AsyncTask works by reading
-                 * http://developer.android.com/reference/android/os/AsyncTask.html
-                 *
-                 */
-        private class ServerTask extends AsyncTask<ServerSocket, String, Void> {
+    private class ServerTask extends AsyncTask<ServerSocket, String, Void> {
         @Override
         protected Void doInBackground(ServerSocket... sockets) {
-            System.out.print("doBackgrond****"+sockets);
             ServerSocket serverSocket = sockets[0];
-            System.out.print("In ServerTask doInBackground*****:"+serverSocket);
+
             /*
              * TODO: Fill in your server code that receives messages and passes them
              * to onProgressUpdate().
              */
+
             Socket socket = null;
             String msg;
 
-            do {
+            // do {
+            while (true)
                 try {
                     // Accept Connection and Initialize Input Stream
                     socket = serverSocket.accept();
-                    System.out.println("Server is listening*****");
                     ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
 
                     msg = (String) input.readObject();
-                    System.out.println("In server msg Received****"+msg);
-                    // Call this to update your progress
+                    System.out.println("Server Message Received::::::" + msg);
                     publishProgress(msg);
                 } catch (IOException e) {
                     Log.e(TAG, "ServerTask IOException");
                 } catch (ClassNotFoundException e) {
                     Log.e(TAG, "ServerTask ClassNotFoundException");
                 }
-            } while(!socket.isInputShutdown());
-
-            return null;
+            //} while(!socket.isInputShutdown());
         }
+        //return null;
 
-        protected void onProgressUpdate(String...strings) {
+
+        protected void onProgressUpdate(String... strings) {
+            System.out.println("String***");
             /*
              * The following code displays what is received in doInBackground().
              */
             String strReceived = strings[0].trim();
             TextView remoteTextView = (TextView) findViewById(R.id.remote_text_display);
             remoteTextView.append(strReceived + "\t\n");
+            remoteTextView.setGravity(Gravity.BOTTOM);
+
             TextView localTextView = (TextView) findViewById(R.id.local_text_display);
             localTextView.append("\n");
 
@@ -238,38 +166,81 @@ public class MainActivity extends Activity
 
             return;
         }
+
     }
 
-    /***
-     * ClientTask is an AsyncTask that should send a string over the network.
-     * It is created by ClientTask.executeOnExecutor() call whenever OnKeyListener.onKey() detects
-     * an enter key press event.
-     *
-     */
+
+
+
+    public int getPort(){
+        return SERVER_PORT;
+    }
+    public String getIpAddress() {
+        String ip = "";
+        try {
+            Enumeration<NetworkInterface> enumNetworkInterfaces = NetworkInterface
+                    .getNetworkInterfaces();
+            while (enumNetworkInterfaces.hasMoreElements()) {
+                NetworkInterface networkInterface = enumNetworkInterfaces
+                        .nextElement();
+                Enumeration<InetAddress> enumInetAddress = networkInterface
+                        .getInetAddresses();
+                while (enumInetAddress.hasMoreElements()) {
+                    InetAddress inetAddress = enumInetAddress
+                            .nextElement();
+
+                    if (inetAddress.isSiteLocalAddress()) {
+                        ip += "Server running at : "
+                                + inetAddress.getHostAddress();
+                    }
+                }
+            }
+
+        } catch (SocketException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            ip += "Something Wrong! " + e.toString() + "\n";
+        }
+        return ip;
+    }
+
+    // Create an anonymous implementation of OnClickListener
+    private View.OnClickListener sendMessage = new View.OnClickListener() {
+        public void onClick(View v) {
+            // do something when the button is clicked
+            // Yes we will handle click here but which button clicked??? We don't know
+            String message = editText.getText().toString()+"\n";
+            editText.setText("");
+            System.out.println("MESSAGE ON CLICK***" + message);
+            textView.append("\t" + message);
+            textView.setGravity(Gravity.BOTTOM);
+
+
+
+            new ClientTask().executeOnExecutor(AsyncTask.SERIAL_EXECUTOR, message, REMOTE_PORT0);
+        }
+    };
+
     private class ClientTask extends AsyncTask<String, Void, Void> {
 
         @Override
         protected Void doInBackground(String... msgs) {
-            System.out.println("In ClientTask doInBackground method called");
+            Socket socket = null;
+            System.out.println("In Client Task doInBackground method****"+msgs);
             try {
                 String remotePort = REMOTE_PORT0;
                 if (msgs[1].equals(REMOTE_PORT0))
                     remotePort = REMOTE_PORT1;
 
-                String msgToSend = msgs[0];
-                System.out.println("In client doBackground ***"+msgToSend);
-
-                //Socket socket = new Socket(InetAddress.getByAddress(new byte[]{10, 0, 2, 2}),
-                Socket socket = new Socket(InetAddress.getByAddress(new byte[]{127, 0, 0, 1}),
+                socket = new Socket(InetAddress.getByAddress(new byte[]{10, 0, 2, 15}),
                         Integer.parseInt(remotePort));
 
+                String msgToSend = msgs[0];
+
+                /** TODO: Fill in your client code that sends out a message.*/
 
 
-                /*
-                 * TODO: Fill in your client code that sends out a message.
-                 */
-
-                // Initialize Output Stream
+                //Initialize Output Stream
                 ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
 
                 output.writeObject(msgToSend);
@@ -280,8 +251,8 @@ public class MainActivity extends Activity
                 Log.e(TAG, "ClientTask UnknownHostException");
             } catch (IOException e) {
                 Log.e(TAG, "ClientTask socket IOException",e);
-            } catch(Exception e){
-                System.out.println("ERROR in client***" + e);
+            } catch (Exception e){
+                Log.e(TAG,"Exception",e);
             }
 
             return null;
